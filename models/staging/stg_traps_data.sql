6ecb41e2344e785aa9ad3f40d2697416a09152d2
Jack Toke <jack.toke@rhythmix.com.au>
2025-07-29 08:37:55 +0000

    Successfully connected to BigQuery

---
+++ models/staging/stg_traps_data.sql
@@ -0,0 +1,19 @@
+WITH traps AS (
+    SELECT
+        *
+    FROM {{ source("gcp_qut_traps_data", "raw_traps")}}
+)
+SELECT
+    id,
+    'when' AS uploaded_at,
+    activityType AS activity_type,
+    strikeTime AS strike_at,
+    batchId AS batch_id,
+    createdBy AS created_by,
+    trapId AS trap_id,
+    latitude,
+    longitude,
+    batteryLevel AS battery_level,
+    hasImage AS has_image,
+    trapInstalledAt AS installed_at
+FROM traps        
